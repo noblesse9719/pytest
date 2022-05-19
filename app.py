e7e4f647adbcbd9a67b82cc8c7e5e865aa58c911
EC2 Default User <ec2-user@ip-172-31-25-236.us-east-2.compute.internal>
2022-05-19 08:50:01 +0000

    test

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import mysql.connector
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return "some alien world!" 
+
+@app.route('/db')
+def dbquery():
+  mydb = mysql.connector.connect(host="18.220.248.20",user="pytest",password="pytest",database="pytest")
+  mycursor = mydb.cursor()
+  mycursor.execute("select * from test")
+  myresult = mycursor.fetchall()
+  row_headers=[x[0] for x in mycursor.description]
+  json_data=[]
+  for result in myresult:
+      json_data.append(dict(zip(row_headers,result)))
+  return json.dumps(json_data)
+
 
 def fc(x):
     return x.capitalize()
